3475987dc49f1f47db88cf82ffbd96fd17606c61
Marisol2201 <1529@holbertonschool.com>
2020-02-06 03:20:31 +0000

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+put("Size of a char: 1 byte(s);
+Size of an int: 4 byte(s);
+Size of a long int: 4 byte(s);
+Size of a long long int: 8 byte(s);
+Size of a float: 4 byte(s)";
+return (0);
+}

b1c952725d82e3269a2eb1bc6cd19c03a984cb5a
Marisol2201 <1529@holbertonschool.com>
2020-02-07 02:58:11 +0000

    print_comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,31 +6,31 @@
  */
 int main(void)
 {
-  int c = 0;
-  int f;
-  int m;
-  int l;
+int c = 0;
+int f;
+int m;
+int l;
 
-  while (c <= 999)
-    {
-      f = (c / 100 + '0');
-      m = (c / 10 % 10 + '0');
-      l = (c % 10 + '0');
+while (c <= 999)
+{
+f = (c / 100 + '0');
+m = (c / 10 % 10 + '0');
+l = (c % 10 + '0');
 
-      if ((f < m) && (m < l))
-	{
-	  putchar(f);
-	  putchar(m);
-	  putchar(l);
+if ((f < m)&&(m < l))
+{
+putchar(f);
+putchar(m);
+putchar(l);
 
-	  if (c != 789)
-	    {
-	      putchar(',');
-	      putchar(' ');
-	    }
-	}
-      c++;
-    }
-  putchar('\n');
-  return (0);
+if (c != 789)
+{
+putchar(',');
+putchar(' ');
+}
+}
+c++;
+}
+putchar('\n');
+return (0);
 }

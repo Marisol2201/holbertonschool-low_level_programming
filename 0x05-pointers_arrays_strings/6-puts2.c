f90b8c7ceff6032285bfcc7124f0dd66f0b3a03f
Marisol2201 <1529@holbertonschool.com>
2020-02-18 18:51:51 +0000

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * puts2 - check the code for Holberton School students.
+ * @str: The character to print
+ * The character to print
+ * Return: Always 0.
+ */
+
+void puts2(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a = a + 2)
+	{
+			_putchar(str[a]);
+	}
+	_putchar('\n');
+
+}

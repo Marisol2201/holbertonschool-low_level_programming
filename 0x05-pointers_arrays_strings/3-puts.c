f91b958b5680cba751ddb99ce3ef1ecec3c0b483
Marisol2201 <1529@holbertonschool.com>
2020-02-18 15:19:37 +0000

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _puts - writes the character c to stdout
+ * @str: The character to print
+ * Return: n
+ * On error, -1 is returned, and error is set appropriately.
+ */
+
+void _puts(char *str)
+{
+	int x;
+
+	for (x = 0; str[x] != '0'; x++)
+	{
+		_putchar(str[x]);
+	}
+	_putchar('\n');
+}

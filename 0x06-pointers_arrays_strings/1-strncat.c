6e7cafe3d772a60355e3983bc737611bf2472d89
Marisol2201 <1529@holbertonschool.com>
2020-02-20 20:29:26 +0000

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * swap_int - check the code for Holberton School students.
+ * @dest: The character to print
+ * @src: The character to print
+ * @n: The character to print
+ * Return: Always 0.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int a, b;
+
+	for (a = 0; dest[a] != '\0'; a++)
+	{
+	}
+	for (b = 0; b < n && src[b] != '\0'; b++, a++)
+
+		dest[a] = src[b];
+
+	dest[a] = '\0';
+	return (dest);
+}

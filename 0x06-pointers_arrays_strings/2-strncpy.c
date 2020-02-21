3e8e35d14c1374a4cfdf51285b14174179ba2a2e
Marisol2201 <1529@holbertonschool.com>
2020-02-21 13:08:23 +0000

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strncpy - check the code for Holberton School students.
+ * @dest: The character to print
+ * @src: The character to print
+ * @n: The character to print
+ *Return: Always 0.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+	return (dest);
+}

3be8c55e6a01288d18d038165c0924819024671d
Marisol2201 <1529@holbertonschool.com>
2020-02-21 20:35:27 +0000

    string_toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * string_toupper - check the code for Holberton School students.
+ * @x: The character to print
+ * The character to print
+ * Return: Always 0.
+ */
+
+char *string_toupper(char *x)
+{
+	int i = 0;
+
+	while (x[i] != '\0')
+	{
+		if (x[i] >= 'a' && x[i] <= 'z')
+		{
+			x[i] = x[i] - 32;
+		}
+		++i;
+	}
+	return (x);
+}

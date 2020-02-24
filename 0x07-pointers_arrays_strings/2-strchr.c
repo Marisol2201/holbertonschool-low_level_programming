58af8c2e3634723065a601481015b0d911de0880
Marisol2201 <1529@holbertonschool.com>
2020-02-24 19:29:54 +0000

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  * _strchr - check the code for Holberton School students.
@@ -17,5 +18,5 @@ char *_strchr(char *s, char c)
 	}
 	if (c == '\0')
 		return (s);
-	return ('\0');
+	return (NULL);
 }

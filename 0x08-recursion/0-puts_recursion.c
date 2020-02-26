32c3e95592e890546e99cf2b52c6dd6659327130
Marisol2201 <1529@holbertonschool.com>
2020-02-26 19:16:36 +0000

    puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,13 +9,12 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s)
-	{
-		_putchar('\n');
-	}
-	else
+	if (*s != '\0')
 	{
 		_putchar(*s);
-		_puts_recursion(s + 1);
+		s++;
+		_puts_recursion(s);
 	}
+	else
+		_putchar('\n');
 }

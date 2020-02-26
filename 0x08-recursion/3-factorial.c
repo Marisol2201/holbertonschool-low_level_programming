c6a2f4c416b8d0af696c6badeaac68af347d4edd
Marisol2201 <1529@holbertonschool.com>
2020-02-26 21:13:27 +0000

    factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,8 +1,17 @@
 #include "holberton.h"
 
 /**
- * swap_int - check the code for Holberton School students.
- * @a: The character to print
- * @b: The character to print
+ * factorial - check the code for Holberton School students.
+ * @n: The character to print
+ * The character to print
  * Return: Always 0.
  */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}

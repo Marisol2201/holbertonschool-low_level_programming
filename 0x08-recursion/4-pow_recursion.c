749f8cf6ac22bb4b2123b6233b21b65fcf1ea32f
Marisol2201 <1529@holbertonschool.com>
2020-02-26 21:55:58 +0000

    pow_recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,8 +1,17 @@
 #include "holberton.h"
 
 /**
- * swap_int - check the code for Holberton School students.
- * @a: The character to print
- * @b: The character to print
+ * _pow_recursion - check the code for Holberton School students.
+ * @x: The character to print
+ * @y: The character to print
  * Return: Always 0.
  */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y -1));
+}

2e2790c1615893845d6e4e2ff3e709f9adf4f7cc
Marisol2201 <1529@holbertonschool.com>
2020-02-26 14:40:56 +0000

    puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,8 +1,20 @@
 #include "holberton.h"
 
 /**
- * swap_int - check the code for Holberton School students.
- * @a: The character to print
- * @b: The character to print
+ * _puts_recursion - check the code for Holberton School students.
+ * @s: The character to print
+ * The character to print
  * Return: Always 0.
  */
+
+void _puts_recursion(char *s)
+{
+	int i;
+
+	if (*s)
+	{
+		for (i = 0; i[s] != '\0'; i++)
+		_putchar(i[s]);
+		_putchar('\n');
+	}
+}

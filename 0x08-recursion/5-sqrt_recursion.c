4f7ac6c959a0c5cc68ff739b4a35cbcf9e3504c1
Marisol2201 <1529@holbertonschool.com>
2020-02-27 12:58:43 +0000

    sqrt_recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,8 +1,35 @@
 #include "holberton.h"
 
 /**
- * swap_int - check the code for Holberton School students.
+ * _natural_square - check the code for Holberton School students.
  * @a: The character to print
  * @b: The character to print
  * Return: Always 0.
  */
+
+int _natural_square(int a, int b)
+{
+	if (a == (b * b))
+		return (b);
+	if (b  * b  >  a)
+		return (-1);
+	return (_natural_square(a, b + 1));
+}
+
+/**
+ * _sqrt_recursion - check the code for Holberton School students.
+ * @n: The character to print
+ * The character to print
+ * Return: Always 0.
+ */
+
+int _sqrt_recursion(int n)
+{
+ 	if (n == 1)
+ 		return (1);
+ 	if (n == 0)
+ 		return (0);
+ 	if (n  < 1)
+ 		return (-1);
+ 	return (_natural_square(n, 1));
+}

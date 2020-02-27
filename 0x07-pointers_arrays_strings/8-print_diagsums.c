c620709fc9e668237042d4e4fdc811793dd2bda7
Marisol2201 <1529@holbertonschool.com>
2020-02-27 02:54:08 +0000

    print_diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,29 +2,22 @@
 #include <stdio.h>
 
 /**
- * swap_int - check the code for Holberton School students.
+ * print_diagsums - check the code for Holberton School students.
  * @a: The character to print
- * @b: The character to print
+ * @size: The character to print
  * Return: Always 0.
  */
 
 void print_diagsums(int *a, int size)
 {
-	int i, j;
-	int sum = 0;
+	int x, y, suma, sumb;
 
-		for (i = 0; i < size; i++)
-		{
-			for (j =0; j < size; j++)
-			{
-				if (i == j)
-				{
-				        sum  += a[i][j];
-					printf("%d", sum);
-				}
-				putchar(',');
-				putchar(' ');
-			}
-		}
-		putchar('\n');
+	suma = 0;
+	sumb = 0;
+
+	for (x = 0;  x < size; x++)
+		suma += a[(size + 1) * x];
+	for (y =  0; y < size; y++)
+		sumb += a[(size - 1) * (y + 1)];
+	printf("%d, %d\n",  suma, sumb);
 }

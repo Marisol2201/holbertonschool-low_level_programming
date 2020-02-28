5e54f841ede1fcd5acf3c168484f1cb7d2e658de
Marisol2201 <1529@holbertonschool.com>
2020-02-28 02:24:57 +0000

    is_prime_number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,8 +1,29 @@
 #include "holberton.h"
 
 /**
- * swap_int - check the code for Holberton School students.
+ * is_prime - check the code for Holberton School students.
  * @a: The character to print
  * @b: The character to print
  * Return: Always 0.
  */
+
+int is_prime(int a, int b)
+{
+	if (a < 2)
+		return (0);
+	if (b * b > a)
+		return (1);
+	return (a % b == 0 ? 0 : (is_prime(a, b + 1)));
+}
+
+/**
+ * is_prime_number - check the code for Holberton School students.
+ * @n: The character to print
+ * The character to print
+ * Return: Always 0.
+ */
+
+int is_prime_number(int n)
+{
+	return (is_prime(n, 2));
+}

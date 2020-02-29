fea1d9c0e710c8584b5972e939a6a7cf7ccd086f
Marisol2201 <1529@holbertonschool.com>
2020-02-29 23:51:59 +0000

    args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,11 +9,7 @@
 
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-	int i;
 
-	for (i = 0; i < argc; i++)
-	{
-		printf("%d\n", argc - 1);
-	}
+	printf("%d\n", argc - 1);
 	return (0);
 }

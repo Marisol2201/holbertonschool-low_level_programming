a24b934011434156d5284aa8e00c47acc40b41ff
Marisol2201 <1529@holbertonschool.com>
2020-03-11 18:25:08 +0000

    Function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - check the code for Holberton School students.
+ * @name: The character to print
+ * @f: The character to print
+ * Return: Always 0.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+		f(name);
+}

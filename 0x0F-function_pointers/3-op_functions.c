c47fa40b9db724f55c77feda866027541cc10276
Marisol2201 <1529@holbertonschool.com>
2020-03-13 03:51:19 +0000

    op_functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include "calc.h"
+#include <stdlib.h>
+
+/**
+ * op_add - sum.
+ * @a: The character to print
+ * @b: The character to print
+ * Return: Always 0.
+ */
+
+int op_add(int a, int b)
+{
+	return(a + b);
+}
+
+/**
+ * op_add - sub.
+ * @a: The character to print
+ * @b: The character to print
+ * Return: Always 0.
+ */
+
+int op_sub(int a, int b)
+{
+	return(a - b);
+}
+
+/**
+ * op_mul - mul.
+ * @a: The character to print
+ * @b: The character to print
+ * Return: Always 0.
+ */
+
+int op_mul(int a, int b)
+{
+	return(a * b);
+}
+
+/**
+ * op_div - div.
+ * @a: The character to print
+ * @b: The character to print
+ * Return: Always 0.
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b)
+}
+
+/**
+ * op_mod -mod
+ * @a: The character to print
+ * @b: The character to print
+ * Return: Always 0.
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b)
+}

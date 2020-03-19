16180c45dcfd2125a52cae04d6c62e6dff3fe5a8
Marisol2201 <1529@holbertonschool.com>
2020-03-19 23:32:24 +0000

    Task 2 v0

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_node - f that adds a new node at the beginning of a list_t list
+ * @str: pointer to string input
+ * @head: pointer to pointer of list_t struct type
+ * Return: Always 0.
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *temp = NULL;
+	unsigned int counter;
+
+	temp = malloc(sizeof(list_t));
+	if (temp == NULL)
+		return (NULL);
+	for (counter = 0; str[counter] != '\0'; counter++)
+	temp->len = counter;
+	temp->str = strdup(str);
+	temp->next = *head;
+	*head = temp;
+	return (*head);
+}

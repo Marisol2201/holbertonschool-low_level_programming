cd8603d6fecdccfc6c0dfbe6f62aa42448d5f51b
Marisol2201 <1529@holbertonschool.com>
2020-03-19 23:45:01 +0000

    Task 2 v1

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,6 +4,22 @@
 #include "lists.h"
 
 /**
+ * _strlen - writes the character c to stdout
+ * @s: The character to print
+ * Return: n
+ * On error, -1 is returned, and error is set appropriately.
+ */
+
+int _strlen(char *s)
+{
+	int n;
+
+	for (n = 0; *s != '\0'; s++)
+		n++;
+	return (n);
+}
+
+/**
  * add_node - f that adds a new node at the beginning of a list_t list
  * @str: pointer to string input
  * @head: pointer to pointer of list_t struct type

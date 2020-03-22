56378933b8b2b7719ba07a7dfdac82c3c77161c8
Marisol2201 <1529@holbertonschool.com>
2020-03-22 00:13:58 +0000

    Task 3 v0

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,33 @@
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_node_end - function that adds a new node at the end of a list_t list 
+ * @head: dasfasfs
+ * @str: asdasdasd
+ * return: afsas
+*/
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+  list_t *temp;
+  int counter = 0;
+
+  temp = malloc(sizeof(list_t));
+  if (temp == NULL)
+    return (NULL);
+  if (head == NULL)
+    *head = temp;
+  while (str[counter] != '\0')
+    {
+      counter++;
+    }
+  temp->str = strdup(str);
+  temp-> len = counter;
+  temp->next = *head;
+  *head = temp;
+  return (*head);
+}

14e939a1e4abf3ceaf15922d3c0a2d4a1e81712e
Marisol2201 <1529@holbertonschool.com>
2020-03-22 23:15:59 +0000

    Task 3 new pointer to first element

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -14,20 +14,31 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *temp;
-	int counter = 0;
+	list_t *first;
+	unsigned int counter;
 
-	temp = malloc(sizeof(list_t));
-	if (temp == NULL)
-		return (NULL);
-	if (head == NULL)
-		*head = temp;
+	counter = 0;
 	while (str[counter] != '\0')
 	{
 		counter++;
 	}
-	temp->str = strdup(str);
-	temp->len = counter;
-	temp->next = *head;
-	*head = temp;
+
+	first = malloc(sizeof(list_t));
+	if (first == NULL)
+		return (NULL);
+	first->str = strdup(str);
+	first->len = counter;
+	if (*head == NULL)
+	{
+		*head = first;
+		first->next = NULL;
+		return (*head);
+	}
+	temp = *head;
+	while (temp->next != NULL)
+		temp = temp->next;
+	temp->next = first;
+	first->next = NULL;
+
 	return (*head);
 }

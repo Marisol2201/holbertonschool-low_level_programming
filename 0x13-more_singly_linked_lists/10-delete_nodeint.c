7dc53749ff95bc0c69514c014db4d89cdb9f9c85
Marisol2201 <1529@holbertonschool.com>
2020-03-24 23:00:30 +0000

    Task 10 v0

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - f that deletes the node at index, index of a list
+ * @head: double pointer to head of linked list
+ * @index: index of the list where the new node should be added
+ *
+ * Return: pointer to the indexed node
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+
+}

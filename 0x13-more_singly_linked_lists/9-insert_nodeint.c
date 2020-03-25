65ea8a9684f824957ce61f28836870ec9a6684f6
Marisol2201 <1529@holbertonschool.com>
2020-03-25 22:02:05 +0000

    change of statements order

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,6 +25,15 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new_node->n = n;
 
+
+	if (idx == 0)
+        {
+                new_node->next = *head;
+		*head = new_node;
+                return (new_node);
+	}
+
+
 	current = *head;
 
 	while (current)
@@ -38,15 +47,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		i++;
 		current = current->next;
 	}
-
-	if (idx == 0)
-	{
-		new_node->next = *head;
-		*head = new_node;
-		return (new_node);
-	}
-
-	new_node->next = current->next;
-	current->next = new_node;
-	return (new_node);
+	return (NULL);
 }

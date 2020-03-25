fbbe2a96033404d4af0085f87f725e60b626b5a0
Marisol2201 <1529@holbertonschool.com>
2020-03-25 01:58:30 +0000

    With while i < idx

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -6,32 +6,45 @@
 /**
  * *insert_nodeint_at_index - f that inserts a new node at a given position.
  * @head: double pointer to head of linked list
- * @idx: index of the list where the new node should be added
+ * @idx: index of the list where the new node should be added (position)
  * @n: data in the node
  * Return: the address of the new node
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node;
-	listint_t *temp;
+	listint_t *new_node, *current;
+	unsigned int i;
+
+	if (head == NULL)
+		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
+
 	new_node->n = n;
 	new_node->next = NULL;
 
-	if (*head == NULL)
+	current = *head;
+	while (i < idx)
 	{
+		if (current == NULL)
+		{
+			return (NULL);
+		}
+		current = current->next;
+		i++;
+	}
+
+	if (idx == 0)
+	{
+		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
 	}
-	temp = *head;
-	while (temp->next != NULL)
-	{
-		temp = temp->next;
-	}
-	temp->next = new_node;
+
+	new_node->next = current->next;
+	current->next = new_node;
 	return (new_node);
 }

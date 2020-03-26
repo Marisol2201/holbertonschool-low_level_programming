1d4d6de7a933e93249b60bb64ea6aafea4ce7b19
Marisol2201 <1529@holbertonschool.com>
2020-03-26 23:19:42 +0000

    with recursion

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,14 +10,14 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int i = 1;
+	unsigned long int count = 1;
 
 	if (n > 1)
 	{
 		print_binary(n >> 1);
 	}
-	if ((n & i) == 1)
+	if ((n & count) == 1)
 		_putchar('1');
-	if ((n & i) == 0)
+	if ((n & count) == 0)
 		_putchar('0');
 }

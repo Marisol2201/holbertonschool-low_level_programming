7c0bb74a5ce71705e3bca41a3852540ce6cdce65
Marisol2201 <1529@holbertonschool.com>
2020-03-31 23:11:56 +0000

    creat function instead of open

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, len, bytes_write;
 
-	fd = creat(filename, 0600);
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (fd == -1)
 		return (-1);
 

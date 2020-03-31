2d48d103b8d22c8a5572169a34d759828b2ae26d
Marisol2201 <1529@holbertonschool.com>
2020-03-31 23:09:21 +0000

    creat function

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, len, bytes_write;
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fd = creat(filename, 0600);
 	if (fd == -1)
 		return (-1);
 

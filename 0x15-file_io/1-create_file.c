8849b28c5dfb7e220e6ac72c418075d9443b4344
Marisol2201 <1529@holbertonschool.com>
2020-03-31 23:00:05 +0000

    if (fd == -1)

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * create_file - function that creates a file.
+ * @filename: name of the file to create
+ * @text_content: string to be written into file
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len, bytes_write;
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	for (len = 0; text_content[len]; len++)
+		;
+	bytes_write = write(fd, text_content, len);
+	if (bytes_write == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}

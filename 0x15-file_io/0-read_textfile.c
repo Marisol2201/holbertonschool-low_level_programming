c3766d616d1eee5c2fda9a2165fc63a02789049c
Marisol2201 <1529@holbertonschool.com>
2020-03-31 20:26:16 +0000

    without variable to close

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,41 +11,29 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;/*integer value returned by the open function*/
 	char *buf;/*the characters are being read*/
-	ssize_t num_bytes_r, num_bytes_w, num_bytes_c;
+	ssize_t num_bytes_r, num_bytes_w;
 
 	if (filename == NULL)
 		return (0);
 
-	buf = malloc(sizeof(char) * letters);
-	if (buf == NULL)
-		return (-1);
-
 	fd = open("filename", O_RDONLY);/*way to open file*/
 	if (fd == -1)
 	{
+                return (0);
+        }
+
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
 		return (0);
-	}
 
 	num_bytes_r = read(fd, buf, letters);
 	if (num_bytes_r == -1)
-	{
-		free(buf);
 		return (0);
-	}
 
 	num_bytes_w = write(STDIN_FILENO, buf, num_bytes_r);
 	if (num_bytes_w == -1)
-	{
-		free(buf);
 		return (0);
-	}
 
-	num_bytes_c = close(fd);
-	if (num_bytes_c == -1)
-	{
-		return (-1);
-	}
-
-	free(buf);
+	close(fd);
 	return (num_bytes_r);
 }

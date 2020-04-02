29848bc60d7731c8989d9ab82c95a9d3b340af54
Marisol2201 <1529@holbertonschool.com>
2020-04-02 00:56:03 +0000

    betty

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,43 +9,42 @@
 
 int main(int argc, char *argv[])
 {
-	int file_from, file_to, bytes_write, bytes_read, bytes_close;
-	char buffer[1024];
+	int file_from, file_to, i, c, r, w;
+	char buf[1024];
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
-
+	for (i = 0; i < 1024; i++)
+		buf[i] = 0;
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
-			argv[1]), exit(98);
-
-	file_to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
-
-	for (bytes_read = 1024; bytes_read == 1024;)
+	r = read(file_from, buf, 1024);
+	if (r == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	while (r)
 	{
-		bytes_read = read(file_from, buffer, 1024);
-		if (bytes_read == -1)
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
-				argv[1]), exit(98);
-
-		bytes_write = write(file_to, buffer, 1024);
-		if (bytes_write == -1)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
-				argv[2]), exit(99);
-	}
-
-	bytes_close = close(file_from);
-	if (bytes_close == -1)
+		w = write(file_to, buf, r);
+		if (w == -1)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		r = read(file_from, buf, 1024);
+		if (r == -1)
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+	c = close(file_from);
+	if (c == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
-
-	bytes_close = close(file_to);
-	if (bytes_close == -1)
+	c = close(file_to);
+	if (c == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
 		exit(100);
 	return (0);

49b2b2fb5d7a2416bcdd862c8485b315d29b0352
Marisol2201 <1529@holbertonschool.com>
2020-04-02 00:59:59 +0000

    } to close the while

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -39,6 +39,7 @@ int main(int argc, char *argv[])
 		if (r == -1)
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
+	}
 	c = close(file_from);
 	if (c == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);

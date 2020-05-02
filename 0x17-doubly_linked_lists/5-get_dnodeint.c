31ddf4b59b6b307f984ae3237293b70fb928efe4
Marisol2201 <1529@holbertonschool.com>
2020-05-02 19:11:40 +0000

    (current = current->next) after (count == index)

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -16,8 +16,8 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	{
 		if (count == index)
 			return (current);
-                current = current->next;        
-                        count++;        
+		current = current->next;
+		count++;
 	}
 	return (current);
 }

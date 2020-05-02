d6bb1bc718c4f27ae2c3fce47e09b0caa4f6076d
Marisol2201 <1529@holbertonschool.com>
2020-05-02 23:06:24 +0000

    if (count < idx) condition

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -42,5 +42,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		count++;
 		current = current->next;
 	}
+	if (count < idx)
+	{
+		free(new);
+		return (NULL);
+	}
 	return (NULL);
 }

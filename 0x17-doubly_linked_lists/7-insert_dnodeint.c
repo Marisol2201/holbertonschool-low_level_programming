d7ddb9ed11069735323ffec63c5ad8d89534bc0e
Marisol2201 <1529@holbertonschool.com>
2020-05-03 02:15:30 +0000

    I don't know if it is logical

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -4,43 +4,44 @@
  * insert_dnodeint_at_index - inserts a new node at a given position
  * @h: pointer to the head of a dlist
  * @idx: position
- * @n: int(data)
+ * @n: data for new node
  * Return: address of the new node, or NULL if it failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new = NULL, *current = NULL;
 	unsigned int count = 1;
+	dlistint_t *temp = NULL, *new = NULL;
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL || h == NULL)
 		return (NULL);
-	if (*h == NULL)
+	new->n = n;
+	temp = *h;
+	if (idx == 0)/*add node at beginnig*/
 	{
 		*h = new;
+		new->next = temp;
+		new->prev = NULL;
+		temp->prev = new;
 		return (new);
 	}
-	current = *h, new->n = n;
-	if (idx == 0) /*add node at the beginning of the list*/
-	{
-		*h = new, new->next = current;
-		new->prev = NULL, current->prev = new;
-		return (new);
-	}
-	while (current->next) /*node at the middle*/
+	while (temp->next != NULL) /*add node at some position*/
 	{
 		if (count == idx)
 		{
-			new->prev = current;
-			new->next = current->next;
-			current->next = new, new->next->prev = new;
+			new->prev = temp;
+			new->next = temp->next;
+			temp->next = new;
+			new->next->prev = new;
 		}
-		current = current->next, count++;
+		temp = temp->next;
+		count++;
 	}
-	if (count == idx) /*node at the end*/
+	if (count == idx)/*add node at end*/
 	{
-		new->prev = current;
-		new->next = NULL, current->next = new;
+		new->prev = temp;
+		new->next = NULL;
+		temp->next = new;
 	}
 	if (count < idx)
 	{

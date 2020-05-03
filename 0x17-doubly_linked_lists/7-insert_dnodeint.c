8ae3d9a4fd9475e00f634969ad97279e415ed87b
Marisol2201 <1529@holbertonschool.com>
2020-05-03 02:36:56 +0000

    the last version again

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -43,10 +43,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new->next = NULL;
 		current->next = new;
 	}
-	if (count < idx)
+	if (!*h)
 	{
-		free(new);
-		return (NULL);
+		*h = new;
+		return (new);
 	}
 	return (NULL);
 }

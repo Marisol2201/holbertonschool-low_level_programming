5c43386212d6dcf0d0fa9675b420dbbfbc90d40d
Marisol2201 <1529@holbertonschool.com>
2020-05-03 00:15:55 +0000

    insert node at the beginning, the middle and the end

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -44,5 +44,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new->next = NULL;
 		current->next = new;
 	}
+	if (!idx)
+	{
+		return (NULL);
+	}
 	return (NULL);
 }

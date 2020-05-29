c565e1f26da7e79d22cb583b0ca1b67def87fb3b
Marisol2201 <1529@holbertonschool.com>
2020-05-29 21:58:02 +0000

    again case with add 3-hash_table_set.ckey

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,11 +12,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *new_node;
 
-	if (!ht || !key)
+	if (!ht || !key || !*key)
 		return (0);
 
 	index = key_index((unsigned char *)key, ht->size);
-
 	new_node = malloc(sizeof(hash_node_t));
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);

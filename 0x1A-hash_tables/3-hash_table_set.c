31c2fb8e3ab09cadf639482cc9293f4f5bbaf497
Marisol2201 <1529@holbertonschool.com>
2020-05-29 15:57:02 +0000

    case when there is no value

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -18,12 +18,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	index = key_index((unsigned char *)key, ht->size);
 
 	new_node = malloc(sizeof(hash_node_t));
-	if (!new_node)
-		return (0);
 
 	new_node->key = strdup(key);
 
 	new_node->value = strdup(value);
+	if (!new_node->value)
+		return (0);
 
 	new_node = ht->array[index];
 

b765a79534851a12adfa69e54dd4e6bceb1b56e0
Marisol2201 <1529@holbertonschool.com>
2020-05-29 15:52:45 +0000

    case when there is no ht or key or value

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,6 +12,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *new_node;
 
+	if (!ht || !key || !*key || !value)
+		return (0);
+
 	index = key_index((unsigned char *)key, ht->size);
 
 	new_node = malloc(sizeof(hash_node_t));

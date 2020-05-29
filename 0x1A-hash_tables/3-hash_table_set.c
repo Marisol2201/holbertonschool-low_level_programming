ca2109001c52003547d3ceb79b7b242ea66ec161
Marisol2201 <1529@holbertonschool.com>
2020-05-29 22:28:50 +0000

    reference to 'head'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,16 +10,30 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new_node;
+	hash_node_t *new_node, *current;
 
 	if (!ht || !key || !*key || !value)
 		return (0);
 
 	index = key_index((unsigned char *)key, ht->size);
+
+	current = ht->array[index];
+
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{/*se comporta igual que head, no se puede perder referencia*/
+			free(current->value);
+			current->value = strdup(value);
+			return (1);
+		}
+		current = current->next;
+	}
+
+
 	new_node = malloc(sizeof(hash_node_t));
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
-	new_node->next = NULL;
 
 	if (!ht->array[index])
 		ht->array[index] = new_node;

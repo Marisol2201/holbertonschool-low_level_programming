65802f708557fc86f8dcbb6e2ea9a613650f2603
Marisol2201 <1529@holbertonschool.com>
2020-06-04 18:12:47 +0000

    without condition when ht->array is equal to NULL

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -11,7 +11,7 @@ void hash_table_print(const hash_table_t *ht)
 	hash_node_t *node = NULL;
 	unsigned long int i;
 
-	if (ht == NULL || ht->array == NULL)
+	if (ht == NULL)
 		return;
 
 	printf("{");
@@ -22,7 +22,8 @@ void hash_table_print(const hash_table_t *ht)
 		{
 			printf("\'%s\': \'%s\'", node->key, node->value);
 			node = node->next;
-			printf(", ");
+			if (node)
+				printf(", ");
 		}
 	}
 	printf("}\n");

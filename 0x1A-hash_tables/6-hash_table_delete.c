5165209b9037d9b753acbd3f9a0dad93d71897ea
Marisol2201 <1529@holbertonschool.com>
2020-06-04 18:22:30 +0000

    task 6 v0

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table.
+ * @ht: hash table
+ * Return: void
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *tmp;
+	unsigned long int i;
+
+	if (!ht)
+		return;
+
+	while (index < ht->size)
+	{
+		while (ht->array[index])
+		{
+			tmp = ht->array[index];
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
+			ht->array[index] = ht->array[index]->next;
+		}
+
+		index++;
+	}
+
+	free(ht->array);
+	free(ht);
+}

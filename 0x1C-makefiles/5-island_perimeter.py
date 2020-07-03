f4ea93b4e758ec81b17ba8097cc53dca6349e0ad
Marisol2201 <1529@holbertonschool.com>
2020-07-03 23:58:17 +0000

    with function len

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,15 +5,19 @@
 def island_perimeter(grid):
     """returns the perimeter of the island described in grid"""
     count = 0
-    for iter, row in enumerate(grid):
-        for i, num in enumerate(row):
-            if num == 1:
-                if grid[iter][i - 1] == 0:
-                    count += 1
-                if grid[iter][i + 1] == 0:
-                    count += 1
-                if grid[iter - 1][i] == 0:
-                    count += 1
-                if grid[iter + 1][i] == 0:
-                    count += 1
-    return count
+    # length = len(grid)
+    # length2 = len(grid[0])
+    # print(length, length2)
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                if i == 0 or grid[i - 1][j] == 0:
+                    count += 1  # up
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:
+                    count += 1  # down
+                if j == 0 or grid[i][j - 1] == 0:
+                    count += 1  # left
+                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
+                    count += 1  # right
+    return (count)

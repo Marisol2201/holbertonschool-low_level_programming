8e0a575bf8c72d7c90fe2c020d8988916722c143
Marisol2201 <1529@holbertonschool.com>
2020-07-04 00:02:56 +0000

    deleted code extra

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -12,12 +12,12 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                if i == 0 or grid[i - 1][j] == 0:
+                if grid[i - 1][j] == 0:
                     count += 1  # up
-                if i == len(grid) - 1 or grid[i + 1][j] == 0:
+                if grid[i + 1][j] == 0:
                     count += 1  # down
-                if j == 0 or grid[i][j - 1] == 0:
+                if grid[i][j - 1] == 0:
                     count += 1  # left
-                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
+                if grid[i][j + 1] == 0:
                     count += 1  # right
     return (count)

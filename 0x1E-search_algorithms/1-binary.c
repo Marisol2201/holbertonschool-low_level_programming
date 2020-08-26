3e6239af4e921f8a96876f303f8e9d79da473f11
Marisol2201 <1529@holbertonschool.com>
2020-08-26 20:38:57 +0000

    tasks 0 to 7 v0

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,38 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value in a sorted array of integers using
+ * ...the Binary search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: the number of elements in array
+ * @value: the value to search for
+ * Return: the index where value is located
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+    size_t i;
+    size_t min = 0;
+    size_t max = size - 1;
+    size_t mid = 0;
+
+    while (min <= max)
+    {
+        printf("Searching in array: ");
+        for (i = min; i <= max; i++)
+        {
+            if (i != max)
+			    printf("%d, ", array[i]);
+		    else
+			    printf("%d\n", array[i]);
+        }
+        mid = (min + max) / 2;
+        if (array[mid] == value)
+            return(mid);
+        else if (array[mid] < value)
+            min = mid + 1;
+        if (array[mid] > value)
+            max = mid - 1;
+    }
+    return (-1);
+}
---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,34 @@
+#include "search_algos.h"
+
+/**
+ * jump_search - searches for a value in a sorted array of integers using
+ * ...the Jump search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: the number of elements in array
+ * @value: the value to search for
+ * Return: the index where value is located
+ */
+
+int jump_search(int *array, size_t size, int value)
+{
+    size_t i;
+    size_t block_size = sqrt(size);
+    int start = 0;
+    size_t end = block_size;
+    
+    while (array[end] <= value && end < size)
+    {   
+        printf("Value checked array[%i] = [%i]\n", start, start);
+        start = end; /*it is not correct block then shift block*/
+        end += sqrt(size);
+        if (end > size -1)
+            end = size; /*if right exceeds then bound the range*/
+    }
+    
+    for (i = start; i < end; i++) /*linear search in selected block*/
+    {
+        if (array[i] == value)
+            return (i);
+    }
+    return (-1);
+}

34f09f09e58165b1cc935c2d4fec9530899e53fd
Marisol2201 <1529@holbertonschool.com>
2020-08-26 20:57:16 +0000

    case when array is null added

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -16,6 +16,9 @@ int binary_search(int *array, size_t size, int value)
 	size_t max = size - 1;
 	size_t mid = 0;
 
+	if (array == NULL)
+		return (-1);
+
 	while (min <= max)
 	{
 		printf("Searching in array: ");

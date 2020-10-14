77f2431a4bdbb64dcfe23abfbb0173a9208f8f1f
Marisol2201 <1529@holbertonschool.com>
2020-10-14 23:47:30 +0000

    advanced

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,34 +1,38 @@
 #include "search_algos.h"
+#include <math.h>
 
 /**
- * jump_search - searches for a value in a sorted array of integers using
- * ...the Jump search algorithm
- * @array: pointer to the first element of the array to search in
- * @size: the number of elements in array
- * @value: the value to search for
- * Return: the index where value is located
+ * jump_search - searches using the Jump search algorithm
+ *
+ * @array: array of integers to be traversed
+ * @size: size of the array
+ * @value: value to be searched for
+ * Return: integer, index found
  */
-
 int jump_search(int *array, size_t size, int value)
 {
-	size_t i;
-	size_t block_size = sqrt(size);
-	int start = 0;
-	size_t end = block_size;
+  size_t i, j, m;
 
-	while (array[end] <= value && end < size)
-	{
-		printf("Value checked array[%i] = [%i]\n", start, start);
-		start = end; /*it is not correct block then shift block*/
-		end += sqrt(size);
-		if (end > size - 1)
-			end = size; /*if right exceeds then bound the range*/
-	}
+  m = (size_t)sqrt(size);
 
-	for (i = start; i < end; i++) /*linear search in selected block*/
-	{
-		if (array[i] == value)
-			return (i);
-	}
-	return (-1);
+  if (!array)
+    return (-1);
+  i = 0;
+  while (i * m < size)
+    {
+      if (array[i * m] >= value)
+	break;
+      printf("Value checked array[%li] = [%d]\n", i * m, array[i * m]);
+      i++;
+    }
+
+  printf("Value found between indexes [%li] and [%li]\n", (i - 1) * m, i * m);
+
+  for (j = (i - 1) * m; j <= i * m && j < size; j++)
+    {
+      printf("Value checked array[%li] = [%d]\n", j, array[j]);
+      if (array[j] == value)
+	return (j);
+    }
+  return (-1);
 }
---
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -0,0 +1,51 @@
+#include "search_algos.h"
+#include <math.h>
+
+/**
+ * jump_list - searches using the Jump search algorithm
+ *
+ * @list: linked list of integers to be traversed
+ * @size: size of the array
+ * @value: value to be searched for
+ * Return: integer, index found
+ */
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+  size_t i, j, m;
+  listint_t *temp, *current;
+
+  if (!list)
+    return (NULL);
+  m = (size_t)sqrt(size);
+  i = m, temp = list, current = list;
+  while (i < size)
+    {
+      for (j = 0; j < m; j++)
+	temp = temp->next;
+      printf("Value checked at index [%li] = [%d]\n", temp->index, temp->n);
+      if (temp->n >= value)
+	break;
+      if (temp->next)
+	current = temp;
+      i += m;
+    }
+  if (i >= size && temp->next)
+    {
+      while (temp->next)
+	temp = temp->next;
+      printf("Value checked at index [%li] = [%d]\n", temp->index, temp->n);
+    }
+  if (current != temp)
+    {
+      printf("Value found between indexes [%li] and [%li]\n",
+	     current->index, temp->index);
+    }
+  while (current != temp->next)
+    {
+      printf("Value checked at index [%li] = [%d]\n", current->index, current->n);
+      if (current->n == value)
+	return (current);
+      current = current->next;
+    }
+  return (NULL);
+}
